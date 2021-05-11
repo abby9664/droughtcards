0d0a928829260e1c8375bc5a0b917633d9aafca7
Avi <>
2021-05-11 02:41:02 +0000

    Add images

--- components/card.js
+++ components/card.js
@@ -1,11 +1,14 @@
-import { Card, Heading } from 'theme-ui'
+import { Card, Heading, Image } from 'theme-ui'
 import Markdown from 'markdown-to-jsx';
 
 const TradingCard = ({
-	id, date, location, notes, cost
+	id, date, location, notes, cost, image
 }) => (
 	<Card>
 		<Heading>{location}, {date}</Heading>
+		{image.map(url => (
+			<Image src={url}></Image>
+		))}
 		<Markdown options={{wrapper: 'Text'}}>{notes}</Markdown>
 		<Heading as="subheading">Stats</Heading>
 		<Markdown options={{wrapper: 'Text'}}>{cost}</Markdown>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import TradingCard from '../components/card'
-import { Container, Grid } from 'theme-ui'
+import { Container, Grid, Heading } from 'theme-ui'
 
 export default function HomePage({ data }) {
 	return (
 		<Container>
+		<Heading variant="ultratitle">Droughts</Heading>
+		<br />
 		<Grid columns={[null, 1, 2]} gap={[3, 4]} sx={{ mb: [3, 4] }}>
               {data.map(card => (
 				<TradingCard {...card} key={card.id} />
