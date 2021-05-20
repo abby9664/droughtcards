344d7ccebe333bdc59ac7484117970f33ac21721
Avi <>
2021-05-20 05:55:01 +0000

    Add more icons & color scheme

--- components/card.js
+++ components/card.js
@@ -1,8 +1,8 @@
 import { Card, Heading, Image, Text } from 'theme-ui'
-import React, { Component } from 'react';
 import Markdown from 'markdown-to-jsx';
 import { useState } from 'react'
 import { IoIosStats } from 'react-icons/io'
+import { IoLocation } from 'react-icons/io5'
 import { GiDeadWood } from "react-icons/gi";
 
 const TradingCard = ({
@@ -20,45 +20,56 @@ const TradingCard = ({
 			borderColor: 'primary',
 			background: toggle ? 'white' : `url(${image[0]})`,
 			transform: `rotate(${Math.random() * 6 - 3}deg)`,
-			color: 'black',
+			color: 'dork',
 			minHeight: '60vh',
 		}} onClick={handleClick}>
 			{
 				toggle ? 
 				<>
 					<Heading sx={{
-						
-					}}>{location}, {date}</Heading>
+						mb: [3],
+						display: 'flex',
+						alignItems: 'center',
+					
+					}}><IoLocation size={35} style={{
+						color: '#AF5246',
+					}}/><Text sx={{ml: [2]}}>{location}, {date}</Text></Heading>
 					<Image src={image[1]} sx={{
 						width: '60%',
 						height: 'auto',
+						display: 'block',
 						mx: 'auto',
 					}} />
 			<Markdown options={{ wrapper: 'Text' }}>{notes}</Markdown>
 			<Heading sx={{
 				fontSize: [2],
 				fontWeight: 'bold',
+				display: 'flex',
+				alignItems: 'center',
 				mb: 0,
 				top: 0,
 				left: 0,
-				color: 'primary'
-			}}><IoIosStats />Stats</Heading>
+				color: 'accent'
+			}}><IoIosStats size={28} /><Text sx={{ml: [1]}}>Stats</Text></Heading>
 			<Markdown options={{ wrapper: 'Text' }}>{cost}</Markdown>
 				</> 
 				:
 				<>
 				<Heading sx={{
-					color: 'blue',
+					color: 'white',
 					fontSize: [4],
 					mb: [1],
-					background: '#f00',
+					backgroundColor: 'accent',
 					display: 'relative',
 					width: '100%',
 					p: [2],
-				}}><GiDeadWood style={{
-					color: '#ff0',
-					size: [2],
-				}}/> History of Droughts</Heading>
+					lineHeight: [2],
+					display: 'flex',
+				}}><GiDeadWood size={56} style={{
+					color: '#D48044',
+				}}/> <Text sx={{
+					ml: [2]
+				}}>History of Droughts</Text></Heading>
 				<div style={{height: '30em'}}></div>
 					<Text
 					sx={{
@@ -68,8 +79,8 @@ const TradingCard = ({
 						bottom: 0,
 						left: 0,
 						fontSize: [3],
-						color: 'white',
-						background: '#f00',
+						color: 'card',
+						backgroundColor: 'primary',
 						width: '100%'
 					}}>{location}, {date}</Text>
 				</>
